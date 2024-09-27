01aec61144240127763e04b1b2373e6ccf4acecd
Luismi0202 <gdluis@iesrafaelalberti.es>
2024-09-27 20:08:40 +0200

    Realizada correción de ejercicios 8 y 9 con sus respectivos comentarios.

--- src/ej08.py
+++ src/ej08.py
@@ -2,9 +2,11 @@
 #Escribir el programa del ejercicio 1.7 usando solamente dos variables diferentes.
 
 print ("Dame 3 números")
-num1 = float(input())
-num2 = float(input())
-suma= num1 + num2
-num2 = float(input())
-suma= suma + num2
+#Declaramos una variable temporal a 0 para que luego podamos realizar las sumas de los 3 inputs en un mismo bucle.
+suma = 0
+#Hacemos bucle para que nos pida 3 inputs. En el bucle siempre se suma 1 porque python resta siempre 1 al ultimo valor.
+for i in range (1,3 + 1):
+    num = float(input())
+    num = suma + num
+    suma = num 
 print (f"¡¡MAGIA!! la suma de los 3 números es {suma}")
--- src/ej09.py
+++ src/ej09.py
@@ -2,5 +2,5 @@
 #¿Es posible escribir el programa del ejercicio 1.7 sin usar variables? Inténtalo.
 
 print ("Dame 3 números")
-suma = float(input()) + float(input()) + float(input())
-print (f"¡¡MAGIA!! la suma de los 3 números es {suma}")
+#Imprimimos la suma de los 3 números que nos pongan
+print (f"¡¡MAGIA!! la suma de los 3 números es {float(input()) + float(input()) + float(input())}")
