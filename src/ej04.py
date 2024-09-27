dfc54504e6946994800dbc8a559e40740c0b97df
Luismi0202 <gdluis@iesrafaelalberti.es>
2024-09-27 19:16:04 +0200

    Cambio en el ejercicio 4 para que nos de los grados redondeados

--- src/ej04.py
+++ src/ej04.py
@@ -1,4 +1,5 @@
 print("Dame un número")
 Celsius = float(input())
-Fahrenheit= Celsius * 1.8 + 32 
-print(f"{Celsius} en grados Fahrenheit son {Fahrenheit}")
+#Usamos el round para redondear el resultado de la operación a dos decimales.
+Fahrenheit= round(Celsius * 1.8 + 32, 2)
+print(f"{Celsius} grados celsius en grados Fahrenheit son {Fahrenheit}")
