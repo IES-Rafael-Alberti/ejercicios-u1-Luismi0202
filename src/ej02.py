84189d9145e23550f6ed730b500e0106fc52276a
Luismi0202 <gdluis@iesrafaelalberti.es>
2024-10-01 20:47:10 +0200

    Hecha algunas comprobaciones en algunos ejercicios mientras corregíamos

--- src/ej02.py
+++ src/ej02.py
@@ -1,4 +1,4 @@
-#Ejercicio 1.2.2¶
+#Ejercicio 1.2.2
 #Escribe un programa para pedirle al usuario las horas de trabajo y el precio por hora y calcule el importe total del servicio.
 
 horas = int (input("Horas de trabajo: "))
