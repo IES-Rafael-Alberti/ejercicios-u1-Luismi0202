39ae223ced1625dbae0e7c3a6d9b97409883317a
Luis Miguel Gomila Dominguez <gdluis@iesrafaaelalberti.es>
2024-10-06 23:19:20 +0200

    EJERCICIO 30 HECHO

--- src/ej30.py
+++ src/ej30.py
@@ -2,23 +2,33 @@
 
 #El incremento y el total deben ser mayores que cero. Si no es así, el programa debe finalizar con un error o obligar a que introduzcan un valor correcto para ambos (os lo dejo a vuestra elección, la primera opción es más fácil, aunque el mundo está lleno de valientes).
 #Por ejemplo, si introducen los valores 1, 1 y 10, el programa mostrará en consola exactamente lo siguiente: SERIE => 1-2..3..4..5..6..7..8..9-10
-X=0
-cadena = 0
-print("Dame un número")
-num = int(input())
-while (X==0):
-    print("Dame un incremento")
-    incremento = int(input())
-    print("Dame el total de números que quieres")
-    total = int(input())
-    if (incremento <=0 or total <=0):
-        print("ERROR, INTRODUZCA UN INCREMENTO Y UN TOTAL QUE NO SEA MENOR QUE 0")
-    else:
-        X=1
-    for i in range (num, total+1):
-        if i == num:
-            print(f"{i}-")
-        if i == total:
-            print(f"-{i}")
+def incremento(num,aumento,total):
+    X=0
+    cadena = "SERIE =>"
+    while(X==0):
+        if(aumento <=0 and total <=0):
+            print("ERROR, INTRODUCE DE NUEVO INCREMENTO Y TOTAL, NINGUNO DE LOS DOS DEBE SER MENOR O IGUAL A 0")
+            aumento = int(input("Introduce incremento"))
+            total = int(input("Introduce numero final de la cadena"))
         else:
-            print(f"{i + incremento}...")
+            X=1
+            for i in range (num,total +1, aumento):
+                if i == num:
+                    cadena += str(i) + "-"
+                else:
+                    if i == total:
+                        cadena += "-" + str(i)
+                    else:
+                        cadena +=".." + str(i)
+            print(cadena)
+def main():
+    print("Dame un número")
+    num = int(input())
+    print("Dame un incremento")
+    aumento = int(input())
+    print("Dame un número para acabar la serie")
+    total = int(input())
+    incremento(num,aumento,total)
+
+if __name__ == "__main__":
+    main()
